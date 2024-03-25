27d0f55b811579487fac32c79c70b870db3af4a0
tnetherton13 <111311727+tnetherton13@users.noreply.github.com>
2024-03-25 13:08:59 -0400

    creating branch

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -18,7 +18,7 @@ function VerifyVisit() {
             dob: dob
         };
 
-        fetch('http://127.0.0.1:5000/api/verify', {
+        fetch('https://cptest-vip.utmck.edu:9443/dev/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
