e983881e81610f81895e8819f4ebc8ebebf4b016
jcutmck <jclutter@utmck.edu>
2024-05-03 15:48:59 -0400

    commit on main

--- src/pages/SdohForm.js
+++ src/pages/SdohForm.js
@@ -5,7 +5,8 @@ const SdohForm = () => {
 return(
     <div>
         Your visit has been successfully verified! Please complete any forms listed below before your visit begins.
-        <a href="https://UTMC.formstack.com/forms/utmc_social_needs_assessment_tool" title="Online Form">Online Form - UTMC Social Needs Assessment Tool</a>
+        <a href="https://UTMC.formstack.com/forms/utmc_social_needs_assessment_tool" title="Online Form"
+        style={{ fontWeight: 'bold', color: 'darkblue' }}>UTMC Social Needs Assessment Tool</a>
     </div>
 )
 };  
