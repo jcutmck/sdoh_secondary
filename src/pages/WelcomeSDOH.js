5ce6cf94c6ce311e5dc9afd399c61ddc65d2b59b
jcutmck <jclutter@utmck.edu>
2024-09-18 16:36:00 -0400

    commit on Minor_aesthetics_108

--- src/pages/WelcomeSDOH.js
+++ src/pages/WelcomeSDOH.js
@@ -10,13 +10,14 @@ const SDOHWelcome = () => {
     navigate('/sdoh'); // Navigate to the home page
   };
 
+  console.log("FE-Ver: VERSION 1.08")
   return (
     <NavigationControl redirectPath="/">
       <div>
-        <h1>Welcome to The University of Tennessee Medical Center!</h1>
-        <h4>Many living and working conditions affect a person's health. At The University of Tennessee Medical Center, we want to better understand our patients. What is it like where they live, where they work and where they play.</h4> 
-        <h4>Knowing these environmental factors improves our care and our ability to help.</h4>
-        <h3>Would you answer a few questions about your life?</h3>
+        <h1 className="ml-4">Welcome to The University of Tennessee Medical Center!</h1>
+        <h4 className="ml-4">Many living and working conditions affect a person's health. At The University of Tennessee Medical Center, we want to better understand our patients. What is it like where they live, where they work and where they play.</h4> 
+        <h4 className="ml-4">Knowing these environmental factors improves our care and our ability to help.</h4>
+        <h3 className="ml-4">Would you answer a few questions about your life?</h3>
         <SubmitButton text="Proceed" onClick={handleHome} />
       </div>
     </NavigationControl>
